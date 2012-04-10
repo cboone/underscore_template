14c2ae108a989721262b00302aa8805e968431ed
Christopher Boone <chris@hypsometry.com>
2012-04-09 21:42:18 -0700

    bundle gem rails_underscore_templates

---
+++ lib/rails_underscore_templates.rb
@@ -0,0 +1,5 @@
+require "rails_underscore_templates/version"
+
+module RailsUnderscoreTemplates
+  # Your code goes here...
+end
---
+++ lib/rails_underscore_templates/version.rb
@@ -0,0 +1,3 @@
+module RailsUnderscoreTemplates
+  VERSION = "0.0.1"
+end
